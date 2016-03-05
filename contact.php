c6ba3d8c4fc9e81adb252399a62f26858cdeb0b1
lfulbel <lfulbel@iastate.edu>
2016-03-04 22:24:29 -0600

    fixed error with members nnot showing links

--- contact.php
+++ contact.php
@@ -1,16 +1,16 @@
 <?php
   $name = strip_tags($_POST['name']);
   $visitor_email = strip_tags($_POST['email']);
-  $message = htmlentities(strip_tags( $_POST['message']));
+  $message = htmlentities(strip_tags($_POST['message']));
 
-  $email_subject = "New Form submission from %s", $name;
-  $email_body = "We've recieved a new message: \n". $message . "\n";
+  $email_subject = "New Form submission from ".$name."";
+  $email_body = "We've recieved a new message: \n".$message."\n";
 
 
   $to = "isuwebdevclub@gmail.com";
-  $headers = "From:".$visitor_email."\r\n";
-  $headers .= "Reply-To:".$visitor_email."\r\n";
-  mail($to,$email_subject,$email_body,$headers);
+
+
+  mail($to,$email_subject,$email_body);
 
   header("Location: /");
   exit(0);
