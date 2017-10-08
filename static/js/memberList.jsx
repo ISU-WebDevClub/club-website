ac3489dde9b63e3884f8b8d1ee4de6cf60e9f553
johnjago <johnjago@protonmail.com>
2017-10-08 18:26:30 -0500

    Update content for fall 2017, improve consistency of wording, and fix broken links

--- static/js/memberList.jsx
+++ static/js/memberList.jsx
@@ -79,7 +79,6 @@ var Member = React.createClass({
         content: React.PropTypes.shape({
             description: React.PropTypes.string.isRequired,
             image: React.PropTypes.string.isRequired,
-            link: React.PropTypes.string.isRequired
         }).isRequired,
         name: React.PropTypes.string
     },
@@ -89,7 +88,6 @@ var Member = React.createClass({
             content: {
                 description: '',
                 image: '',
-                link: ''
             },
             name: ''
         };
@@ -101,7 +99,7 @@ var Member = React.createClass({
                 <div className="thumbnail member-thumbnail">
                     <img src={this.props.content.image} className="img-circle" alt="picture api broken" style={{width: '200px', height: '200px'}} />
                     <div className="caption">
-                        <h3 style={{marginTop: '5px'}}><a href={this.props.content.link} >{this.props.name}</a></h3>
+                        <h3 style={{marginTop: '5px'}}>{this.props.name}</h3>
                         <p>{this.props.content.description}</p>
                     </div>
                 </div>
